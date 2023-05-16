8912b69968fb76c4c989fdfad77b76f008c17703
aein <aaeinnor11@gmail.com>
2023-05-16 20:36:15 +0800

    feat: Add test_fetch_address
    Added and finished the implementation of test_fetch_address

--- tests/test_fetch_address.py
+++ tests/test_fetch_address.py
@@ -1,14 +1,96 @@
-from address_scraper.address_scraper.fetch_address import fetch_address, parse_address, generate_coords
-import pytest
+from address_scraper.address_scraper.fetch_address import fetch_address
+from address_scraper.address_scraper.payload import url, payload, headers
+from unittest.mock import Mock, patch
 
 
 def test_fetch_address():
-    ...
+    """
+    Test case for testing the successful execution of fetch_address.
+    """
+    state_code = "TX"
+    expected_result = [
+        {
+            "city": "Colorado City",
+            "line": "840 Chestnut St",
+            "street_name": "Chestnut",
+            "street_number": "840",
+            "street_suffix": "St",
+            "country": "USA",
+            "postal_code": "79512",
+            "state_code": "TX",
+            "state": "Texas",
+            "coordinates": "32.395367,-100.863693",
+            "lat": 32.395367,
+            "lon": -100.863693
+        },
+        {
+            "city": "Canyon",
+            "line": "606 College St",
+            "street_name": "College",
+            "street_number": "606",
+            "street_suffix": "St",
+            "country": "USA",
+            "postal_code": "79015",
+            "state_code": "TX",
+            "state": "Texas",
+            "coordinates": "34.977005,-101.91490967",
+            "lat": 34.977005,
+            "lon": -101.91490967
+        }
+    ]
 
+    mock_response = Mock()
+    mock_response.json.return_value = {
+        "data": {
+            "home_search": {
+                "results": [
+                    {
+                        "location": {
+                            "address": {
+                                "city": "Colorado City",
+                                "line": "840 Chestnut St",
+                                "street_name": "Chestnut",
+                                "street_number": "840",
+                                "street_suffix": "St",
+                                "country": "USA",
+                                "postal_code": "79512",
+                                "state_code": "TX",
+                                "state": "Texas",
+                                "coordinate": {
+                                        "lat": 32.395367,
+                                        "lon": -100.863693
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "location": {
+                            "address": {
+                                "city": "Canyon",
+                                "line": "606 College St",
+                                "street_name": "College",
+                                "street_number": "606",
+                                "street_suffix": "St",
+                                "country": "USA",
+                                "postal_code": "79015",
+                                "state_code": "TX",
+                                "state": "Texas",
+                                "coordinate": {
+                                        "lat": 34.977005,
+                                        "lon": -101.91490967
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    }
 
-def test_parse_address():
-    ...
+    with patch("requests.post", return_value=mock_response) as mock_post:
+        result = fetch_address(state_code)
 
+        mock_post.assert_called_once_with(
+            url, json=payload, headers=headers)
 
-def test_generate_coords():
-    ...
+        assert result == expected_result
