c1376b189539b89bb6fc8f5ad62a8e4296ef03e1
aein <aaeinnor11@gmail.com>
2023-05-16 14:05:07 +0800

    feat: Implement fetch_address.py
    Implement the following function in fetch_address.py:
    - fetch_address
    - parse_address
    - generate_coords

--- address_scraper/fetch_address.py
+++ address_scraper/fetch_address.py
@@ -1,33 +1,77 @@
-# NOTE: @aeinnor
 import requests
 from payload import payload, headers, url
 
-# TODO:
+
+def fetch_address(state_code: str, api_key=headers["X-Rapid-API-Key"], payload: dict = payload, headers: dict = headers, url: str = url) -> list:
+    """
+    Fetches addresses from the API.
+
+    Args:
+        state (string): The state input by the user.
+        payload (dictionary): API request payload. 
+        headers (dictionary): API request headers.
+        url (string): API request url.
+
+    Returns: 
+        list: A list of dictionaries that store the addresses.
+    """
+    payload["state_code"] = state_code
+
+    if api_key != headers["X-Rapid-Api"]:
+        headers["X-Rapid-Api"] = api_key
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+
+        data = response.json()
+        index = data["data"]["home_search"]["results"]
+
+        dataset = [parse_address(index)
+                   for _ in index]
+
+        return dataset
+    except (requests.RequestException, requests.ConnectionError, requests.HTTPError, requests.Timeout) as e:
+        return e
 
 
-def fetch_address(state, payload=payload, headers=headers, url=url):
-    '''
-    Fetches address from API
-    '''
-    payload["state_code"] = state
+def parse_address(data: dict) -> dict:
+    """
+    Parses addresses from the API.
 
-    response = requests.request("GET", url, headers=headers, params=payload)
-    dataset = [parse_address(params=entities)
-               for entities in response.json()["properties"]]
+    Args:
+        data (dict): The dictionary that stores data from the JSON request.
 
-    return dataset
+    Returns: 
+        dict: A dictionary that stores the details of the address.
+    """
+    location = data["location"]
+
+    details = {
+        "city": location["city"],
+        "line": location["line"],
+        "street_name": location["street_name"],
+        "street_number": location["street_number"],
+        "street_suffix": location["street_suffix"],
+        "country": location["country"],
+        "postal_code": location["postal_code"],
+        "state_code": location["state_code"],
+        "state": location["state"],
+        "coordinates": generate_coords(location["lat"], location["lon"]),
+        "lat": location["lat"],
+        "lon": location["lon"],
+    }
+    return details
 
 
-# TODO:
+def generate_coords(lat, lon):
+    """
+    Combines latitude and longitude into a coordinates.
 
+    Args:
+        lat (float): The latitude. 
+        lon (float): The longitude.
 
-def parse_address(params="INPUT"):
-    '''
-    Parses address from API
-    '''
-    # TODO: Crawl JSON
-    ...
-
-
-def generate_coords():
-    ...
+    Returns:
+        str: A string that contains the coordinates.
+    """
+    return f"{lat},{lon}"
