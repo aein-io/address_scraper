72d77d3e085eca0e62e256da252b02cd187c826f
Aaron Nicholas Gumapac <aarn.gmpc@gmail.com>
2023-05-16 23:54:03 +0800

    feature: added map visualization

--- map.py
+++ map.py
@@ -1,34 +1,34 @@
 import os
+import sys
 import pandas as pd
 from geopy.geocoders import Nominatim
 import folium
 
-# Find the first CSV file in the current directory
 csv_files = [file for file in os.listdir() if file.endswith('.csv')]
 if len(csv_files) > 0:
     csv_file = csv_files[0]
 else:
-    print('No CSV files found in the current directory.')
-    exit()
+    sys.exit("No CSV files found in the current directory.")
 
-# Read addresses from the CSV file into a pandas DataFrame
 df = pd.read_csv(csv_file)
 
-# Initialize the geocoder
 geolocator = Nominatim(user_agent='my_map')
 
-# Create a map object
 map_obj = folium.Map()
 
 # Iterate over the addresses in the DataFrame
 for index, row in df.iterrows():
 
-    address = f"{row['city']}, {row['state']}, {row['street_name']}, {row['street_number']}, {row['lat']}, {row['lon']}"
+    latitude = row['lat']
+    longitude = row['lon']
+    if latitude == 0 or longitude == 0:
+        continue
 
-    # Use geocoder to get the latitude and longitude of the address
-    location = geolocator.geocode(address)
+    coords = f"{latitude}, {longitude}"
 
-    # Add a marker to the map
+    location = geolocator.reverse(coords)
+    address = location.address
+
     folium.Marker(
         location=[location.latitude, location.longitude],
         popup=address
@@ -36,3 +36,5 @@ for index, row in df.iterrows():
 
 # Display the map
 map_obj.save('map.html')
+# open the map
+os.system('open map.html')
