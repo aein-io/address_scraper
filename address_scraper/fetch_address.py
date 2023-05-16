6f302fd6e7b2f10458380c07f82be831354dafbe
aein <aaeinnor11@gmail.com>
2023-05-16 20:35:13 +0800

    fix: fetch_address indexing issues
    Fixed fetch_address indexing issues

--- address_scraper/fetch_address.py
+++ address_scraper/fetch_address.py
@@ -2,12 +2,14 @@ import requests
 from payload import payload, headers, url
 
 
-def fetch_address(state_code: str, api_key=headers["X-Rapid-API-Key"], payload: dict = payload, headers: dict = headers, url: str = url) -> list:
+def fetch_address(state_code: str, limit: int = payload["limit"], api_key: str = headers["X-RapidAPI-Key"], payload: dict = payload, headers: dict = headers, url: str = url) -> list:
     """
     Fetches addresses from the API.
 
     Args:
         state (string): The state input by the user.
+        limit (int): Limit for the API request.
+        api_key (string): The API key for the request.
         payload (dictionary): API request payload. 
         headers (dictionary): API request headers.
         url (string): API request url.
@@ -17,17 +19,20 @@ def fetch_address(state_code: str, api_key=headers["X-Rapid-API-Key"], payload: 
     """
     payload["state_code"] = state_code
 
-    if api_key != headers["X-Rapid-Api"]:
-        headers["X-Rapid-Api"] = api_key
+    if api_key != headers["X-RapidAPI-Key"]:
+        headers["X-RapidAPI-Key"] = api_key
+
+    if limit != payload["limit"]:
+        payload["limit"] = limit
 
     try:
         response = requests.post(url, json=payload, headers=headers)
 
         data = response.json()
-        index = data["data"]["home_search"]["results"]
+        results = data["data"]["home_search"]["results"]
 
-        dataset = [parse_address(index)
-                   for _ in index]
+        dataset = [parse_address(result)
+                   for result in results]
 
         return dataset
     except (requests.RequestException, requests.ConnectionError, requests.HTTPError, requests.Timeout) as e:
@@ -44,28 +49,31 @@ def parse_address(data: dict) -> dict:
     Returns: 
         dict: A dictionary that stores the details of the address.
     """
+    print(data)
     location = data["location"]
+    address = location["address"]
+    coordinate = address["coordinate"]
 
     details = {
-        "city": location["city"],
-        "line": location["line"],
-        "street_name": location["street_name"],
-        "street_number": location["street_number"],
-        "street_suffix": location["street_suffix"],
-        "country": location["country"],
-        "postal_code": location["postal_code"],
-        "state_code": location["state_code"],
-        "state": location["state"],
-        "coordinates": generate_coords(location["coordinate"]["lat"], location["coordinate"]["lon"]),
-        "lat": location["coordinate"]["lat"],
-        "lon": location["coordinate"]["lon"]
+        "city": address["city"],
+        "line": address["line"],
+        "street_name": address["street_name"],
+        "street_number": address["street_number"],
+        "street_suffix": address["street_suffix"],
+        "country": address["country"],
+        "postal_code": address["postal_code"],
+        "state_code": address["state_code"],
+        "state": address["state"],
+        "coordinates": generate_coords(coordinate["lat"], coordinate["lon"]),
+        "lat": coordinate["lat"],
+        "lon": coordinate["lon"]
     }
     return details
 
 
 def generate_coords(lat, lon):
     """
-    Combines latitude and longitude into a coordinates.
+    Combines latitude and longitude into coordinates.
 
     Args:
         lat (float): The latitude. 
