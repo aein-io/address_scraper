7836a9d62318a809025c0eea81d283b32891accc
Aaron Nicholas Gumapac <aarn.gmpc@gmail.com>
2023-05-17 17:52:19 +0800

    feat: added support camelcased state inputs

--- address_scraper/validate_state.py
+++ address_scraper/validate_state.py
@@ -1,4 +1,18 @@
+from inflection import camelize
 from states import states
+
+
+def is_camel(s: str) -> bool:
+    return camelize(s) == s or camelize(s, False) == s
+
+
+def split_camel(s: str) -> str:
+    import re
+
+    strings = re.sub('([A-Z][a-z]+)', r' \1',
+                     re.sub('([A-Z]+)', r' \1', s)).split()
+
+    return " ".join(strings)
 
 
 def get_state_code(state: str):
@@ -12,6 +26,9 @@ def get_state_code(state: str):
         str: The state code.
     """
     code = None
+    if len(state) > 2 and is_camel(state):
+        state = split_camel(state)
+
     for abbrev, name in states.items():
         if name.lower() == state.lower() or abbrev.lower() == state.lower():
             code = abbrev
--- tests/test_validate_state.py
+++ tests/test_validate_state.py
@@ -4,5 +4,6 @@ from address_scraper.address_scraper.validate_state import get_state_code
 def test_get_state_abbrev():
     assert get_state_code('California') == 'CA'
     assert get_state_code('New York') == 'NY'
+    assert get_state_code('NewYork') == 'NY'
     assert get_state_code('NY') == 'NY'
     assert get_state_code('Sin City') is None
